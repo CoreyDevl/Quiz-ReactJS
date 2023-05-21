613f5eda2eb185dfa2f5349b526602cc875cea9d
raphaeldevl <coreydaves@hotmail.it>
2023-05-21 19:22:54 -0300

    gerado primeiras condicoes de context para acessar as perguntas do quiz

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,14 +5,14 @@ import './welcome.css'
 
 const Welcome = () => {
 
-  const QuizState = useContext(QuizContext)
-  console.log("QuizState: ", QuizState);
+  const [QuizState, dispatch] = useContext(QuizContext)
+    
    
     return (
     <div id="welcome">
         <h2>Seja Bem-Vindo</h2>
         <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
+        <button onClick={()=>dispatch({type:"CHANGE STATE"})}>Iniciar</button>
         <img src={Quiz} alt="Inicio do quiz" />
     </div>
   )
