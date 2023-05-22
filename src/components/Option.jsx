fa1545c710b1abb6469295102433e9e881469f23
raphaeldevl <coreydaves@hotmail.it>
2023-05-21 22:08:38 -0300

    feito logica de contagem de pontos e logica de bloqueio de pontos infinitos

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -2,12 +2,12 @@ import {useContext} from 'react'
 import { QuizContext } from '../context/quiz'
 import './Option.css'
 
-const Option = ({option}) => {
+const Option = ({option, answer, selectOption}) => {
 
     const [quizState, dispatch] = useContext(QuizContext)
 
   return (
-    <div className='option'>{option}</div>
+    <div className='option' onClick={()=> selectOption()}>{option}</div>
   )
 }
 
--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -7,7 +7,8 @@ const initialState = {
     gameStage: STAGES[0],
     questions,
     currentQuestion: 0,
-    score: 0
+    score: 0,
+    answerSelected: false,
 }
 
 const quizReducer = (state, action) => {
@@ -47,6 +48,22 @@ const quizReducer = (state, action) => {
                     gameStage: endGame ? STAGES[2] : state.gameStage,
                 }
 
+        case "CHECK_ANSWER" :
+            
+            if(state.answerSelected) return state;
+
+            const answer = action.payload.answer
+            const option = action.payload.option
+            let correctAnswer = 0
+            
+            if(answer === option) correctAnswer = 1
+            
+              return{
+                ...state,
+                score: state.score + correctAnswer,
+                answerSelected: option 
+                }
+                
         case "NEW_GAME" :
             
         return initialState;
